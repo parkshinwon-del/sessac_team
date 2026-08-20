3e3b2bda30edc59339a9c70be67a57f54783e09f
guddn20 <128574570+guddn20@users.noreply.github.com>
2026-08-20 12:32:20 +0900

    Update predict.py

--- Classification/predict.py
+++ Classification/predict.py
@@ -3,17 +3,6 @@ from ultralytics import YOLO
 # 학습된 최고 성능 모델
 model = YOLO(r"./classification/runs/detect/food_train01-6/weights/best.pt")
 
-# 예측할 이미지
-#image_path = r"C:\Users\user\Downloads\ssalbap.jpg"
-
-# # 예측
-# results = model.predict(
-#     source=image_path,
-#     conf=0.25,
-#     #save=True,
-#     show=True
-# )
-
 def predict_food(image_path):
     '''
     image_path : 예측할 사진 경로
@@ -52,7 +41,18 @@ def predict_food(image_path):
 # 테스트용
 # ------------------------------------------------------------
 if __name__ == "__main__":
+    # 예측할 이미지
+    # image_path = r"C:\Users\user\Downloads\ssalbap.jpg"
+
+    # # 예측
+    # results = model.predict(
+    #     source=image_path,
+    #     conf=0.25,
+    #     #save=True,
+    #     show=True
+    # )
+    
     image_path = r"C:\Users\user\Downloads\ssalbap.jpg"
 
     food_id, food_name, conf = predict_food(image_path)
-    #print(f"class_id: {food_id}, 예측된 음식: {food_name}, 확신도: {conf:.2f}")
+    # print(f"class_id: {food_id}, 예측된 음식: {food_name}, 확신도: {conf:.2f}")
